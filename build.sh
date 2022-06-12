fd362e96e80a12a4bc610d7bf01f13f5233d718f
ChrisJeong <crowjdh@gmail.com>
2022-06-12 10:14:05 +0100

    Fix base image version for armv7

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+SCRIPT_NAME=`basename "$0"`
+
+if [[ $# -lt 1 ]]; then
+  echo "Usage: $SCRIPT_NAME TOOL_NAME"
+  exit 1
+fi
+
 TOOL_NAME=$1
 # BASE_IMAGE=$2
 
@@ -9,7 +16,7 @@ if [[ $ARCH = "x86_64" ]]; then
   BASE_IMAGE="alpine:3.4"
 elif [[ $ARCH = "armv7l" ]]; then
   # RPI
-  BASE_IMAGE="arm32v7/alpine:3.4"
+  BASE_IMAGE="arm32v7/alpine:3.12.0"
 else
   echo "Unsupported archutecture: $ARCH"
   exit 1
